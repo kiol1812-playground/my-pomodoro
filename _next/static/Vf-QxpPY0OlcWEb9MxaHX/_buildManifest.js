e0a2724a642790cb4cca23b9a39930ab05e0d863
kiol1812 <kioly.vikerg0412@gmail.com>
2026-07-17 23:26:25 +0800

    Updates

--- _next/static/Vf-QxpPY0OlcWEb9MxaHX/_buildManifest.js
+++ _next/static/Vf-QxpPY0OlcWEb9MxaHX/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/my-pomodoro//_next/:path+",
+        "destination": "/my-pomodoro/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
