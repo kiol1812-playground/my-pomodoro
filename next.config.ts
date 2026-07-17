891499776396e2afb949dc50c6729d39976d65f1
kiol1812 <kioly.vikerg0412@gmail.com>
2026-07-17 22:11:16 +0800

    chore: add gh-pages dependency and deployment scripts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+// refer to https://github.com/nextjs/deploy-github-pages
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/my-pomodoro",
 };
 
 export default nextConfig;
